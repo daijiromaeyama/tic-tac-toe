dd92f16a540fb0f9d4eb1696172352938ff395fd
daijiromaeyama <daijiro.maeyama13@gmail.com>
2023-01-17 14:57:40 +0900

    Add winner decision

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ export default function Board(): JSX.Element {
   const [squares, setSquares] = useState<Array<string | null>>(Array(9).fill(null))
 
   function handleClick(i: number): void {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return
     }
     const nextSquares: Array<string | null> = squares.slice()
@@ -30,8 +30,17 @@ export default function Board(): JSX.Element {
     setXIsNext(!xIsNext)
   }
 
+  const winner: string | null = calculateWinner(squares)
+  let status: string
+  if (winner) {
+    status = 'Winner: ' + winner
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O')
+  }
+
   return (
     <>
+      <div className='status'>{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -49,4 +58,24 @@ export default function Board(): JSX.Element {
       </div>
     </>
   )
+}
+
+function calculateWinner(squares: Array<string | null>): string | null {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
 }
